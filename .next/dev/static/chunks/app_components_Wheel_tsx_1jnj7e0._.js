149e8af6d82ebad5c269a13fb18cfab5c82a48e7
Rodrigo Alarcón <rodricsxd@gmail.com>
2026-06-24 18:53:35 -0300

    Remove .next from repository

--- .next/dev/static/chunks/app_components_Wheel_tsx_1jnj7e0._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/app_components_Wheel_tsx_09_n8jf._.js"],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/app_page_tsx_0zr2k9a._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/app_components_Wheel_tsx_0vrq1dj._.js","static/chunks/_06vrp-6._.js"],
-    source: "dynamic"
-});
